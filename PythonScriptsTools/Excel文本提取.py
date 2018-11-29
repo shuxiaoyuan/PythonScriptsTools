6722f74ca2f6120c6d667ceb2bd3419d59754bbf
lishuyuan <lishuyuan@doodlemobile.com>
2018-11-29 13:27:45 +0800

    first commit

---
+++ PythonScriptsTools/Excel文本提取.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+@Author: lishuyuan
+@Date&Time: 2018年11月20日 下午4:48:34
+@Description: 从 Excel 表格中提取文本，然后输出到文本文件中
+'''
+
+import xlrd
+
+path = "level.xlsx"
+excel = xlrd.open_workbook(path)
+sheet = excel.sheets()[0]
+levels = open("levels.txt", 'a', encoding='utf-8') 
+
+for i in range(1, sheet.nrows):
+    levels.write(
+        str(int(sheet.cell_value(i, 0))) + "," +
+        str(sheet.cell_value(i, 1)) + "," +
+        str(sheet.cell_value(i, 2)) + "," +
+        str(sheet.cell_value(i, 3)) + "\n" 
+    )
+        
+levels.close()
---
+++ PythonScriptsTools/词典过滤.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+@Author: lishuyuan
+@Date&Time: 2018年11月20日 下午4:06:14
+@Description: 将原词典 文本文件中长度为 3~7 的单词输出到一个新的文本文件中
+'''
+
+wordlist_filter = open("wordslist_filter.txt", 'a', encoding='utf-8')
+with open("wordslistexport.txt", "r", encoding='utf-8') as f:
+    for line in f.readlines():
+        if len(line.strip()) >= 3 and len(line.strip()) <= 7:
+            wordlist_filter.write(line)
