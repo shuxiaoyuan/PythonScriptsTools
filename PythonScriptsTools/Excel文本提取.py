7e86b10d4bf4a61efcbb8f8d3c8b9b9688458b23
lishuyuan <lishuyuan@doodlemobile.com>
2019-08-09 14:42:31 +0800

    更

--- PythonScriptsTools/Excel文本提取.py
+++ PythonScriptsTools/Excel文本提取.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
-@Author: lishuyuan
-@Date&Time: 2018年11月20日 下午4:48:34
-@Description: 从 Excel 表格中提取文本，然后输出到文本文件中
-'''
+# @Author: lsy
+# @Date&Time: 2019/8/9 10:02
+# @Description: 从 Excel 中提取数据生成关卡
 
 import xlrd
 
-path = "level.xlsx"
+path = "level new.xlsx"
 excel = xlrd.open_workbook(path)
 sheet = excel.sheets()[0]
 with open("levels.txt", "w", encoding="utf-8") as f:
     f.write("")
 levels = open("levels.txt", 'a', encoding='utf-8')
 
-for i in range(1, sheet.nrows):
-    levels.write(
-        str(int(sheet.cell_value(i, 0)) if str.strip(str(sheet.cell_value(i, 0))) else sheet.cell_value(i, 0)) + ", " +
-        str(sheet.cell_value(i, 1)) + ", " +
-        str(sheet.cell_value(i, 2)) + ", " +
-        str(sheet.cell_value(i, 3)) + ", " +
-        str(int(sheet.cell_value(i, 4)) if str.strip(str(sheet.cell_value(i, 4))) else sheet.cell_value(i, 4)) + ", " +
-        str(int(sheet.cell_value(i, 5)) if str.strip(str(sheet.cell_value(i, 5))) else sheet.cell_value(i, 5)) + "\n" 
-    )
+for i in range(1, 31):
+    line = str(int(sheet.cell_value(i, 1))) + ", " + \
+            str(int(sheet.cell_value(i, 6))) + ", " + \
+            str(int(sheet.cell_value(i, 5)) if str.strip(str(sheet.cell_value(i, 5))) else " ") + ", " + \
+            str(sheet.cell_value(i, 2)) + ", " + \
+            str(sheet.cell_value(i, 3)) + ", " + \
+            str(sheet.cell_value(i, 4)) + ", " + "\n"
+    levels.write(line)
+    print(line)
 
 levels.close()
